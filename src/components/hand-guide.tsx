f299d9f663c296fe6b365b65c649ee2a8b4553a3
Mathew <mathewjeen404@gmail.com>
2026-01-26 16:12:24 +0000

    and also can we modify the hand guide for each letters like in typing cl

--- src/components/hand-guide.tsx
+++ src/components/hand-guide.tsx
@@ -17,32 +17,47 @@ const Finger = ({ path, isHighlighted }: { path: string; isHighlighted: boolean 
         )}
         strokeWidth="1.5"
         strokeLinejoin="round"
+        strokeLinecap="round"
     />
 );
 
 export function HandGuide({ highlightFinger }: HandGuideProps) {
     const isThumb = highlightFinger === 'thumb';
+
     return (
-        <div className="flex justify-center items-end gap-4 w-full max-w-xl mx-auto h-40">
+        <div className="flex justify-center items-end gap-2 w-full max-w-2xl mx-auto h-48 -mb-4">
             {/* Left Hand */}
-            <svg viewBox="0 0 123 113" className="h-full w-auto">
+            <svg viewBox="0 0 150 120" className="h-full w-auto">
                 <g>
-                    <path d="M113.5,61.5v-13c0-5.2-4-9.5-9-9.5H89.2c-5,0-9,4.2-9,9.5v32c0,5.2,4,9.5,9,9.5h8.2c5,0,9-4.2,9-9.5V61.5z" className={cn('fill-card-foreground/5 stroke-card-foreground/10 transition-colors duration-150', isThumb && 'fill-accent/50 stroke-accent')} strokeWidth="1.5" strokeLinejoin="round" />
-                    <Finger path="M68.8,88.5H53.2c-5,0-9-4.2-9-9.5V10c0-5.2,4-9.5,9-9.5h15.5c5,0,9,4.2,9,9.5v69C77.8,84.2,73.8,88.5,68.8,88.5z" isHighlighted={highlightFinger === 'left-middle'} />
-                    <Finger path="M44.2,77.5H28.8c-5,0-9-4.2-9-9.5V21c0-5.2,4-9.5,9-9.5h15.5c5,0,9,4.2,9,9.5v47C53.2,73.2,49.2,77.5,44.2,77.5z" isHighlighted={highlightFinger === 'left-ring'} />
-                    <Finger path="M19.8,66.5H4.2c-2.2,0-4-2-4-4.5V32c0-2.5,1.8-4.5,4-4.5h15.5c2.2,0,4,2,4,4.5v30C23.8,64.5,22,66.5,19.8,66.5z" isHighlighted={highlightFinger === 'left-pinky'} />
-                    <Finger path="M92.2,77.5H76.8c-5,0-9-4.2-9-9.5V21c0-5.2,4-9.5,9-9.5h15.5c5,0,9,4.2,9,9.5v47C101.2,73.2,97.2,77.5,92.2,77.5z" isHighlighted={highlightFinger === 'left-index'} />
+                    {/* Palm */}
+                    <path d="M 110 118 C 80 125, 40 120, 20 95 S 10 50, 45 45 S 85 30, 100 50 S 135 60, 125 90 Z" className="fill-card-foreground/5" />
+
+                    {/* Fingers */}
+                    <Finger path="M 28 70 C 20 65, 18 45, 25 30 S 35 12, 42 15 S 50 35, 45 50 S 40 75, 28 70 Z" isHighlighted={highlightFinger === 'left-pinky'} />
+                    <Finger path="M 52 52 C 45 45, 45 25, 53 13 S 65 -2, 72 2 S 82 23, 75 38 S 65 60, 52 52 Z" isHighlighted={highlightFinger === 'left-ring'} />
+                    <Finger path="M 78 45 C 72 38, 72 15, 80 3 S 90 -8, 98 -5 S 108 15, 100 30 S 90 53, 78 45 Z" isHighlighted={highlightFinger === 'left-middle'} />
+                    <Finger path="M 104 53 C 98 48, 98 22, 107 12 S 118 -5, 125 0 S 135 22, 128 35 S 118 60, 104 53 Z" isHighlighted={highlightFinger === 'left-index'} />
+
+                    {/* Thumb */}
+                    <Finger path="M 125 90 C 130 80, 145 80, 150 90 S 150 105, 142 110 S 125 115, 120 105 S 120 95, 125 90 Z" isHighlighted={isThumb} />
+
                 </g>
             </svg>
 
             {/* Right Hand */}
-            <svg viewBox="0 0 123 113" className="h-full w-auto scale-x-[-1]">
+            <svg viewBox="0 0 150 120" className="h-full w-auto scale-x-[-1]">
                 <g>
-                    <path d="M113.5,61.5v-13c0-5.2-4-9.5-9-9.5H89.2c-5,0-9,4.2-9,9.5v32c0,5.2,4,9.5,9,9.5h8.2c5,0,9-4.2,9-9.5V61.5z" className={cn('fill-card-foreground/5 stroke-card-foreground/10 transition-colors duration-150', isThumb && 'fill-accent/50 stroke-accent')} strokeWidth="1.5" strokeLinejoin="round" />
-                    <Finger path="M68.8,88.5H53.2c-5,0-9-4.2-9-9.5V10c0-5.2,4-9.5,9-9.5h15.5c5,0,9,4.2,9,9.5v69C77.8,84.2,73.8,88.5,68.8,88.5z" isHighlighted={highlightFinger === 'right-middle'} />
-                    <Finger path="M44.2,77.5H28.8c-5,0-9-4.2-9-9.5V21c0-5.2,4-9.5,9-9.5h15.5c5,0,9,4.2,9,9.5v47C53.2,73.2,49.2,77.5,44.2,77.5z" isHighlighted={highlightFinger === 'right-ring'} />
-                    <Finger path="M19.8,66.5H4.2c-2.2,0-4-2-4-4.5V32c0-2.5,1.8-4.5,4-4.5h15.5c2.2,0,4,2,4,4.5v30C23.8,64.5,22,66.5,19.8,66.5z" isHighlighted={highlightFinger === 'right-pinky'} />
-                    <Finger path="M92.2,77.5H76.8c-5,0-9-4.2-9-9.5V21c0-5.2,4-9.5,9-9.5h15.5c5,0,9,4.2,9,9.5v47C101.2,73.2,97.2,77.5,92.2,77.5z" isHighlighted={highlightFinger === 'right-index'} />
+                    {/* Palm */}
+                    <path d="M 110 118 C 80 125, 40 120, 20 95 S 10 50, 45 45 S 85 30, 100 50 S 135 60, 125 90 Z" className="fill-card-foreground/5" />
+
+                    {/* Fingers */}
+                    <Finger path="M 28 70 C 20 65, 18 45, 25 30 S 35 12, 42 15 S 50 35, 45 50 S 40 75, 28 70 Z" isHighlighted={highlightFinger === 'right-pinky'} />
+                    <Finger path="M 52 52 C 45 45, 45 25, 53 13 S 65 -2, 72 2 S 82 23, 75 38 S 65 60, 52 52 Z" isHighlighted={highlightFinger === 'right-ring'} />
+                    <Finger path="M 78 45 C 72 38, 72 15, 80 3 S 90 -8, 98 -5 S 108 15, 100 30 S 90 53, 78 45 Z" isHighlighted={highlightFinger === 'right-middle'} />
+                    <Finger path="M 104 53 C 98 48, 98 22, 107 12 S 118 -5, 125 0 S 135 22, 128 35 S 118 60, 104 53 Z" isHighlighted={highlightFinger === 'right-index'} />
+
+                    {/* Thumb */}
+                    <Finger path="M 125 90 C 130 80, 145 80, 150 90 S 150 105, 142 110 S 125 115, 120 105 S 120 95, 125 90 Z" isHighlighted={isThumb} />
                 </g>
             </svg>
         </div>
